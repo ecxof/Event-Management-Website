4caae3318f787619238a8ff119356f2c47479ab7
hillzhang <vezq52431821z@gmail.com>
2026-05-21 05:25:30 +0800

    Write Connection File Between Project and Database

---
+++ connect_db/config.php
@@ -0,0 +1,6 @@
+<?php
+//I will fill up the mongo db rui and database name soon, for now I am just putting the placeholders.
+  return [
+      'mongodb_uri' => 'mongodb+srv://username:password@cluster.mongodb.net/',
+      'database' => 'event_management',
+  ];
---
+++ connect_db/db.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$config = require __DIR__ . '/config.php';
+
+if (str_contains($config['mongodb_uri'], 'USERNAME') || str_contains($config['mongodb_uri'], 'PASSWORD')) {
+    die('Please update mongodb_uri in config.php first.');
+}
+
+try {
+    $client = new MongoDB\Client($config['mongodb_uri']);
+    $db = $client->selectDatabase($config['database']);
+} catch (Throwable $e) {
+    die('MongoDB connection error: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8'));
+}
